88bb53c1c51ac0ec562e043d4a9c92c58ec6c51d
nikithagumpella <nikithagumpella@outlook.in>
2022-05-22 14:37:51 +0530

    menu

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'B19ALPHA'`, () => {
+  it(`should have as title 'project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('B19ALPHA');
+    expect(app.title).toEqual('project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('B19ALPHA app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('project app is running!');
   });
 });
